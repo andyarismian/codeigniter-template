f7035fb1788a83bd65c9c5d42b45e8e82229d7ef
andy arismianto <andyarismian@gmail.com>
2021-08-30 16:36:13 +0700

    add template code-igniter

---
+++ application/libraries/Enkripsi.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Enkripsi {
+  
+	private $skey 	= "3dul4baka2020"; // you can change it
+	
+    public  function safe_b64encode($string) {
+	
+        $data = base64_encode($string);
+        $data = str_replace(array('+','/','='),array('-','_',''),$data);
+        return $data;
+    }
+
+	public function safe_b64decode($string) {
+        $data = str_replace(array('-','_'),array('+','/'),$string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
+    }
+
+}
